64cb1aa7970a4aa93e94a7f2c170f4bf35aae400
EgorKondrashov <egor.condrashov@gmail.com>
2019-12-08 13:27:32 +0300

    Доработал копирайт раздела про безопасмность в соц сетях, добавил плавный скролл к якорям

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+// прелоадер
 document.body.onload = function() {
 
 	setTimeout(function() {
@@ -9,3 +10,20 @@ document.body.onload = function() {
 	}, 1000);
 	
 }
+
+// прелоадер плавный скролл до якоря
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
